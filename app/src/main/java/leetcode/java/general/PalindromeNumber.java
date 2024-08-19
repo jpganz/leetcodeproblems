baaf3727c57109d4828ca3cb228f07e02528ced1
Juan Hernande <jpganz18@gmail.com>
2024-08-19 20:26:56 +0200

    adding a palindrome in number

--- app/src/main/java/leetcode/java/general/PalindromeNumber.java
+++ app/src/main/java/leetcode/java/general/PalindromeNumber.java
@@ -17,7 +17,7 @@ public class PalindromeNumber {
 
     //approach 1: reversing half the number
     public static boolean isPalindrome2(int x) {
-        if(x < 0) {
+        if(x < 0 || (x >= 10 && x % 10 == 0 )) {
             return false;
         } else {
             String equivalent = String.valueOf(x);
@@ -37,28 +37,25 @@ public class PalindromeNumber {
 
     //approach 2: using the modular operator
     public static boolean isPalindrome(int x) {
-        if(x < 0) {
+        if(x < 0 || (x >= 10 && x % 10 == 0 )) {
             return false;
         } else {
             int lastDigit;
             int reversedNumber = x;
             int createdPalindrome = 0;
-            int multiplier = 1;
+            int multiplier = 10;
             while(reversedNumber > 0) {
                 lastDigit = reversedNumber % 10;
-                createdPalindrome += (multiplier * lastDigit);
+                createdPalindrome = (multiplier * createdPalindrome) + lastDigit;
                 reversedNumber /= 10;
-                multiplier *= 10;
-
             }
-            System.out.println(createdPalindrome);
             return (x == createdPalindrome);
         }
     }
 
 
     public static void main(String[] args) {
-        boolean isBoolean = isPalindrome(10);
+        boolean isBoolean = isPalindrome(123); //3 * 10 + 2 = 32  ; 32 * 10 + 1
         System.out.println("Is it palindrome? " + isBoolean);
 
     }
