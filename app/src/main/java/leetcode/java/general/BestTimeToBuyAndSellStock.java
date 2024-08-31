a292f69bd09ea3448fb0d6fa65b394392d05bc50
Juan Hernande <jpganz18@gmail.com>
2024-08-31 08:27:32 +0200

    Kadane solution

---
+++ app/src/main/java/leetcode/java/general/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,30 @@
+package leetcode.java.general;
+
+
+/*
+https://leetcode.com/problems/add-two-numbers/description/
+
+ */
+
+public class BestTimeToBuyAndSellStock {
+
+
+    // Kadane algoritm
+    public static int maxProfit(int[] prices) {
+        int diff = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            diff = Math.max(diff, prices[i] - min);
+            min = Math.min(prices[i], min);
+
+        }
+        return diff;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {7, 1, 5, 3, 6, 4};
+        int output = maxProfit(input);
+        System.out.println(output);
+    }
+}
+
