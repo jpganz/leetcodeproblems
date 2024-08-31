b2830a0b48d5783e378a6983d41d54df4b9f9906
Juan Hernande <jpganz18@gmail.com>
2024-08-31 08:28:25 +0200

    Adding two numbers

--- app/src/main/java/leetcode/java/general/BestTimeToBuyAndSellStock.java
+++ app/src/main/java/leetcode/java/general/BestTimeToBuyAndSellStock.java
@@ -2,8 +2,7 @@ package leetcode.java.general;
 
 
 /*
-https://leetcode.com/problems/add-two-numbers/description/
-
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock
  */
 
 public class BestTimeToBuyAndSellStock {
