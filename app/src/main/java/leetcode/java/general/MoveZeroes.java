71dd93fae392c7e9577499850a7409d2c04262c2
Juan Hernande <jpganz18@gmail.com>
2024-09-15 22:16:18 +0200

    A third solution

--- app/src/main/java/leetcode/java/general/MoveZeroes.java
+++ app/src/main/java/leetcode/java/general/MoveZeroes.java
@@ -6,16 +6,18 @@ https://leetcode.com/problems/move-zeroes/description
  */
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class MoveZeroes {
 
-    public static void moveZeroes(int[] nums) {
-        for(int i = 0; i < nums.length - 1; i++) {
-
-            if(nums[i] == 0) {
+    // 0, 3, 0, 2, 0, 3
+    public static void moveZeroesBubbleSortish(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 0) {
                 int counter = i + 1;
-                while(counter < nums.length) {
-                    if(nums[counter] != 0) {
+                while (counter < nums.length) {
+                    if (nums[counter] != 0) {
                         nums[i] = nums[counter];
                         nums[counter] = 0;
                         break;
@@ -26,11 +28,39 @@ public class MoveZeroes {
         }
     }
 
+    //using a queue is more efficient, but not on the top expected
+    public static void moveZeroesWithQueue(int[] nums) {
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                queue.add(i);
+            } else if (!queue.isEmpty()) {
+                nums[queue.poll()] = nums[i];
+                nums[i] = 0;
+                queue.add(i);
+            }
+        }
+    }
+
+    public static void moveZeroes(int[] nums) {
+        // 1, 2, 0, 3
+        int counterDigits = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != counterDigits) {
+                    nums[counterDigits] = nums[i];
+                    nums[i] = 0;
+                }
+                counterDigits++;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
-      int[] input = {0,1,0,3,12};
-      moveZeroes(input);
-      System.out.println(Arrays.toString(input));
+        int[] input = {2, 1};
+        moveZeroes(input);
+        System.out.println(Arrays.toString(input));
     }
 }
 
