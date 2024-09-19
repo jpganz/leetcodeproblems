abd84dc790f7e0123e953895989211c129c47ef1
Juan Hernande <jpganz18@gmail.com>
2024-09-19 20:51:48 +0200

    Updating variables

--- app/src/main/java/leetcode/java/general/IsSubsequence.java
+++ app/src/main/java/leetcode/java/general/IsSubsequence.java
@@ -6,14 +6,16 @@ https://leetcode.com/problems/string-compression/
  */
 
 
-import java.util.Arrays;
-
 public class IsSubsequence {
 
     public static boolean isSubsequence(String s, String t) {
         int sIndex = 0;
+        if(s.isEmpty()) return true;
         for (int i = 0; i < t.length(); i++) {
-            if(sIndex < s.length() && s.charAt(sIndex) == t.charAt(i)) sIndex++;
+            if (sIndex < s.length() && s.charAt(sIndex) == t.charAt(i)) {
+                sIndex++;
+                if (sIndex == s.length()) return true;
+            }
 
         }
         return sIndex == s.length();
